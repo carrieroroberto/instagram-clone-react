66fe29356a3375192da2c156167e0de85eeb4291
Roberto Carriero <66308934+carrieroroberto@users.noreply.github.com>
2021-12-27 20:44:38 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import HeaderBar from "./components/HeaderBar/HeaderBar";
 import Post from "./components/Post/Post";
 import db from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
+import { Modal, Button, Input } from "@material-ui/core";
 
 export default function App() {
   // state
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   const col = collection(db, "posts");
 
@@ -25,7 +26,27 @@ export default function App() {
   return (
     <div className="app">
       {/* header bar */}
-      <HeaderBar />
+      <div className="header_bar">
+        <img
+          className="header_bar_logo"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="logo"
+        />
+        <div className="header_bar_buttons">
+          <Button onClick={() => setOpen(true)}>Accedi</Button>
+          <Button onClick={() => setOpen(true)}>Registrati</Button>
+        </div>
+      </div>
+
+      {/* modal */}
+      <Modal className="modal" open={open} onClose={() => setOpen(false)}>
+        <div className="modal_content">
+          <Input type="text" placeholder="Inserisci il tuo username" />
+          <Input type="email" placeholder="Inserisci la tua email" />
+          <Input type="password" placeholder="Inserisci la tua password" />
+          <Button>Registrati</Button>
+        </div>
+      </Modal>
 
       {/* posts */}
       {posts.map((post) => (
